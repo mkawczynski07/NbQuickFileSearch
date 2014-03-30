2b4036541edf183cb0ec3a5fd7dd8ae7ddcf8ff3
naza <naza@op.pl>
2014-03-30 11:18:01 +0200

    file change listener example

--- src/pl/nazaweb/netbeans/quickfilesearch/QuickFileSearchAction.java
+++ src/pl/nazaweb/netbeans/quickfilesearch/QuickFileSearchAction.java
@@ -7,12 +7,23 @@ package pl.nazaweb.netbeans.quickfilesearch;
 
 import java.awt.event.ActionEvent;
 import java.awt.event.ActionListener;
+import org.netbeans.api.project.FileOwnerQuery;
+import org.netbeans.api.project.Project;
 import org.netbeans.api.project.ui.OpenProjects;
 import org.openide.awt.ActionID;
 import org.openide.awt.ActionReference;
 import org.openide.awt.ActionRegistration;
+import org.openide.filesystems.FileAttributeEvent;
+import org.openide.filesystems.FileChangeListener;
+import org.openide.filesystems.FileEvent;
 import org.openide.filesystems.FileObject;
+import org.openide.filesystems.FileRenameEvent;
+import org.openide.loaders.DataObject;
+import org.openide.util.Lookup;
+import org.openide.util.LookupEvent;
+import org.openide.util.LookupListener;
 import org.openide.util.NbBundle.Messages;
+import org.openide.util.Utilities;
 
 @ActionID(
         category = "File",
@@ -27,21 +38,58 @@ public final class QuickFileSearchAction implements ActionListener {
 
     @Override
     public void actionPerformed(ActionEvent e) {
+//        OpenProjects.getDefault().getMainProject().getProjectDirectory().addFileChangeListener();
         for (FileObject file : OpenProjects.getDefault().getMainProject().getProjectDirectory().getChildren()) {
             System.out.println(file.getName() + ", path: " + file.getPath());
         }
-//        final Lookup.Result<DataObject> dtoResult = Utilities.actionsGlobalContext().lookupResult(DataObject.class);
-//        dtoResult.addLookupListener(new LookupListener() {
-//
-//            @Override
-//            public void resultChanged(LookupEvent ev) {
-//                for (DataObject dto : dtoResult.allInstances()) {
-//                    Project project = FileOwnerQuery.getOwner(dto.getPrimaryFile());
-//                    if (project != null) {
-//                        System.out.println("File " + dto.getPrimaryFile() + " of Project " + project.getProjectDirectory());
-//                    }
-//                }
-//            }
-//        });
+
+        final Lookup.Result<DataObject> dtoResult = Utilities.actionsGlobalContext().lookupResult(DataObject.class);
+        dtoResult.addLookupListener(new LookupListener() {
+
+            @Override
+            public void resultChanged(LookupEvent ev) {
+                for (DataObject dto : dtoResult.allInstances()) {
+                    Project project = FileOwnerQuery.getOwner(dto.getPrimaryFile());
+                    if (project != null) {
+                        dto.getPrimaryFile().addFileChangeListener(new MyFileChangeListener());
+                        System.out.println("File " + dto.getPrimaryFile() + " of Project " + project.getProjectDirectory());
+                    }
+                }
+            }
+        });
+
+    }
+
+    public class MyFileChangeListener implements FileChangeListener {
+
+        @Override
+        public void fileFolderCreated(FileEvent fe) {
+            System.out.println("folder create");
+        }
+
+        @Override
+        public void fileDataCreated(FileEvent fe) {
+            System.out.println("file create");
+        }
+
+        @Override
+        public void fileChanged(FileEvent fe) {
+            System.out.println("file change");
+        }
+
+        @Override
+        public void fileDeleted(FileEvent fe) {
+            System.out.println("file delete");
+        }
+
+        @Override
+        public void fileRenamed(FileRenameEvent fre) {
+            System.out.println("file rename");
+        }
+
+        @Override
+        public void fileAttributeChanged(FileAttributeEvent fae) {
+            System.out.println("file attr change");
+        }
     }
 }
