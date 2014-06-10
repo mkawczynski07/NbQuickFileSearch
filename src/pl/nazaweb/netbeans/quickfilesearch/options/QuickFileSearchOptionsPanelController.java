40a603f56b7fb3f2aeae4fde8d06394ec0f435ec
jkawczynski <jk70250@gmail.com>
2014-06-10 13:52:08 +0200

    Added empty QuickSearch configuration panel in NetBeans options

---
+++ src/pl/nazaweb/netbeans/quickfilesearch/options/QuickFileSearchOptionsPanelController.java
@@ -0,0 +1,86 @@
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+package pl.nazaweb.netbeans.quickfilesearch.options;
+
+import java.beans.PropertyChangeListener;
+import java.beans.PropertyChangeSupport;
+import javax.swing.JComponent;
+import javax.swing.SwingUtilities;
+import org.netbeans.spi.options.OptionsPanelController;
+import org.openide.util.HelpCtx;
+import org.openide.util.Lookup;
+
+@OptionsPanelController.SubRegistration(
+        displayName = "#AdvancedOption_DisplayName_QuickFileSearch",
+        keywords = "#AdvancedOption_Keywords_QuickFileSearch",
+        keywordsCategory = "Advanced/QuickFileSearch"
+)
+@org.openide.util.NbBundle.Messages({"AdvancedOption_DisplayName_QuickFileSearch=QuickFileSearch", "AdvancedOption_Keywords_QuickFileSearch=QuickFileSearch configuration."})
+public final class QuickFileSearchOptionsPanelController extends OptionsPanelController {
+
+    private QuickFileSearchPanel panel;
+    private final PropertyChangeSupport pcs = new PropertyChangeSupport(this);
+    private boolean changed;
+
+    public void update() {
+        getPanel().load();
+        changed = false;
+    }
+
+    public void applyChanges() {
+        SwingUtilities.invokeLater(new Runnable() {
+            @Override
+            public void run() {
+                getPanel().store();
+                changed = false;
+            }
+        });
+    }
+
+    public void cancel() {
+        // need not do anything special, if no changes have been persisted yet
+    }
+
+    public boolean isValid() {
+        return getPanel().valid();
+    }
+
+    public boolean isChanged() {
+        return changed;
+    }
+
+    public HelpCtx getHelpCtx() {
+        return null; // new HelpCtx("...ID") if you have a help set
+    }
+
+    public JComponent getComponent(Lookup masterLookup) {
+        return getPanel();
+    }
+
+    public void addPropertyChangeListener(PropertyChangeListener l) {
+        pcs.addPropertyChangeListener(l);
+    }
+
+    public void removePropertyChangeListener(PropertyChangeListener l) {
+        pcs.removePropertyChangeListener(l);
+    }
+
+    private QuickFileSearchPanel getPanel() {
+        if (panel == null) {
+            panel = new QuickFileSearchPanel(this);
+        }
+        return panel;
+    }
+
+    void changed() {
+        if (!changed) {
+            changed = true;
+            pcs.firePropertyChange(OptionsPanelController.PROP_CHANGED, false, true);
+        }
+        pcs.firePropertyChange(OptionsPanelController.PROP_VALID, null, null);
+    }
+
+}
