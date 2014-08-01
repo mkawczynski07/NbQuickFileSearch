48ae164155517616405cbf0291dcc09624171bb7
Marek Kawczyński <mkawczynski07@gmail.com>
2014-08-01 15:00:00 +0200

    FileCollector refactoring.

--- src/pl/nazaweb/netbeans/quickfilesearch/files/FileCollector.java
+++ src/pl/nazaweb/netbeans/quickfilesearch/files/FileCollector.java
@@ -1,12 +1,7 @@
 package pl.nazaweb.netbeans.quickfilesearch.files;
 
-import java.io.BufferedWriter;
 import java.io.File;
-import java.io.FileNotFoundException;
-import java.io.FileWriter;
 import java.io.IOException;
-import java.io.PrintWriter;
-import java.io.UnsupportedEncodingException;
 import java.nio.file.Files;
 import java.nio.file.Path;
 import java.nio.file.Paths;
@@ -22,17 +17,21 @@ import org.openide.util.Exceptions;
 public class FileCollector {
 
     public void addProjectFiles(Project project) throws IOException {
-        Files.walk(Paths.get(project.getProjectDirectory().toURI())).filter(filePath
-                -> !shouldIgnoreFileInDirectory(filePath)
-        ).forEach(filePath -> {
-            try {
-                if (Files.isRegularFile(filePath)) {
-                    addFile(FileUtil.createData(filePath.toFile()));
-                }
-            } catch (Exception ex) {
-                Exceptions.printStackTrace(ex);
+        Path projectPath = Paths.get(project.getProjectDirectory().toURI());
+        Files.walk(projectPath).filter(filePath -> !shouldIgnoreFileInDirectory(filePath))
+                .forEach(filePath -> {
+                    tryAddProjectFile(filePath);
+                });
+    }
+
+    private void tryAddProjectFile(Path filePath) {
+        try {
+            if (Files.isRegularFile(filePath)) {
+                addFile(FileUtil.createData(filePath.toFile()));
             }
-        });
+        } catch (Exception ex) {
+            Exceptions.printStackTrace(ex);
+        }
     }
 
     private void addFile(FileObject file) throws Exception {
