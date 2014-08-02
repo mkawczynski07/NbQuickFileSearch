c63f097569c5244d4136b384ad0da5a1358fd17c
Marek Kawczyński <mkawczynski07@gmail.com>
2014-08-02 14:42:18 +0200

    CamelCase search strategy - used only in tests for now ...

--- src/pl/nazaweb/netbeans/quickfilesearch/search/strategy/CamelCase.java
+++ src/pl/nazaweb/netbeans/quickfilesearch/search/strategy/CamelCase.java
@@ -1,5 +1,7 @@
 package pl.nazaweb.netbeans.quickfilesearch.search.strategy;
 
+import java.util.regex.Matcher;
+import java.util.regex.Pattern;
 import pl.nazaweb.netbeans.quickfilesearch.files.FileItem;
 
 /**
@@ -11,7 +13,31 @@ public class CamelCase extends AbstractStrategy implements SearchStrategy {
     @Override
     public boolean test(String text, FileItem file) {
         init(text, file);
-        throw new UnsupportedOperationException();
+        return matchCamelCase(text, file.getName());
+    }
+
+    private boolean matchCamelCase(String query, String str) {
+        StringBuilder queryBuilder = new StringBuilder();
+        for (int i = 0; i < query.length(); i++) {
+            if (Character.isUpperCase(query.charAt(i))) {
+                queryBuilder.append(".*?").append(query.charAt(i));
+            } else if (query.charAt(i) == 42) {
+                queryBuilder.append(".*?");
+            } else {
+                queryBuilder.append(query.charAt(i));
+            }
+        }
+
+        query = queryBuilder.toString();
+
+        String re = "\\b(" + query.replaceAll("([A-Z][^A-Z]*)", "$1[^A-Z]*") + ".*?)\\b";
+
+        Pattern regex = Pattern.compile(re);
+
+        Matcher m = regex.matcher(str);
+        boolean find = m.find();
+        System.out.println(str + " - > " + re + " = " + find);
+        return find;
     }
 
 }
--- test/unit/src/pl/nazaweb/netbeans/quickfilesearch/search/strategy/CamelCaseNGTest.java
+++ test/unit/src/pl/nazaweb/netbeans/quickfilesearch/search/strategy/CamelCaseNGTest.java
@@ -1,8 +1,10 @@
 package pl.nazaweb.netbeans.quickfilesearch.search.strategy;
 
-import java.util.HashMap;
-import java.util.Map;
+import java.io.File;
+import java.util.ArrayList;
+import java.util.List;
 import static org.testng.Assert.*;
+import org.testng.annotations.BeforeMethod;
 import org.testng.annotations.Test;
 import pl.nazaweb.netbeans.quickfilesearch.files.FileItem;
 
@@ -12,23 +14,67 @@ import pl.nazaweb.netbeans.quickfilesearch.files.FileItem;
  */
 public class CamelCaseNGTest {
 
-    private final static Map<String, String> TEST_DATA = new HashMap();
+    private final static List<String> TEST_DATA = new ArrayList();
 
     static {
-//        TEST_DATA.put("", null)
+        TEST_DATA.add("NewClass.java");
+        TEST_DATA.add("NowaKlasa.java");
+        TEST_DATA.add("Order.js");
+        TEST_DATA.add("new_File.conf");
+        TEST_DATA.add("Order.java");
+        TEST_DATA.add("Newworld.java");
+        TEST_DATA.add("NoClassKl.java");
+        TEST_DATA.add("NyxKlo.java");
+    }
+    private CamelCase instance;
+
+    @BeforeMethod
+    public void init() {
+        instance = new CamelCase();
     }
 
     @Test
-    public void testTest() {
-        System.out.println("test");
-        String text = "";
-        FileItem file = null;
-        CamelCase instance = new CamelCase();
-        boolean expResult = false;
-        boolean result = instance.test(text, file);
-        assertEquals(result, expResult);
-        // TODO review the generated test code and remove the default call to fail.
-        fail("The test case is a prototype.");
+    public void shouldReturnOnlyFilesMatchTo_Ne() {
+        assertEquals(testData("Ne"), 2);
+    }
+
+    @Test
+    public void shouldReturnOnlyFilesMatchTo_NC() {
+        assertEquals(testData("NC"), 2);
+    }
+
+    @Test
+    public void shouldReturnOnlyFilesMatchTo_NK() {
+        assertEquals(testData("NK"), 3);
+    }
+
+    public void shouldReturnOnlyFilesMatchTo_NstarK() {
+        assertEquals(testData("N*K"), 3);
+    }
+
+    @Test
+    public void shouldReturnOnlyFilesMatchTo_F() {
+        assertEquals(testData("F"), 1);
+    }
+
+    @Test
+    public void shouldReturnOnlyFilesMatchTo_NeC() {
+        assertEquals(testData("NeC"), 1);
+    }
+
+    @Test
+    public void shouldReturnOnlyJavaFiles() {
+        assertEquals(testData("*.java"), 6);
+    }
+
+    private int testData(String pattern) {
+        int count = 0;
+        count = TEST_DATA.stream()
+                .filter((file)
+                        -> (instance.test(pattern, new FileItem(new File(file), "project"))))
+                .map((_item) -> 1)
+                .reduce(count, Integer::sum);
+        return count;
     }
 
 }
